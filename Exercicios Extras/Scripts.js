b3f39aa262ce01781d59cde617cf5f85a29c117e
Ismael Chaves <ismaelchaves2001@outlook.com>
2023-07-18 14:38:36 -0300

    Relogio Digital

--- Exercicios Extras/Scripts.js
+++ Exercicios Extras/Scripts.js
@@ -39,3 +39,36 @@ function GerarNum() {
         resultado.innerHTML = '<img src="imgs/Pikachu.png">'
     }
 }
+
+// Agora vamos criar um relogio a partir do horario atual do computador do usuario
+
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MeuRelogio").innerText = time;
+  document.getElementById("MeuRelogio").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
